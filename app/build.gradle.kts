c7a93b83da99c4a060339540b6d54fe562e583e5
xiaoxiaoying91 <xiaoxiaoying91@gmail.com>
2025-01-02 11:33:12 +0800

    change module name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    implementation(project(":pwdview"))
-    implementation(project(":pwdviewcompose"))
+    implementation(project(":view"))
+    implementation(project(":compose"))
+
 }
